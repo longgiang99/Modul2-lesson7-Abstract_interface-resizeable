04ecd8a69525bb0593929471927e9c87be236584
trieu <longgiang9999.gl@gmail.com>
2023-03-30 16:16:31 +0700

    su dung interface- risizeable de thay doi kich thuoc cua cac lop hinh hoc

---
+++ src/interface_resizeable/Resizeable.java
@@ -0,0 +1,5 @@
+package interface_resizeable;
+
+ interface Resizeable {
+     void resize(double percent);
+}
---
+++ src/interface_resizeable/test.java
@@ -0,0 +1,39 @@
+package interface_resizeable;
+
+public class test {
+    public static void main(String[] args) {
+        Shape shape = new Shape();
+        System.out.println(shape);
+        shape = new Shape("red", false);
+        System.out.println(shape);
+
+
+
+
+        Circle circle = new Circle(5);
+        System.out.println(circle);
+        System.out.println(circle.getArea());
+
+        circle.resize(50);
+        System.out.println(circle.getRadius());
+        System.out.println(circle.getArea());
+        circle.resize(-50);
+        System.out.println(circle.getRadius());
+        System.out.println(circle.getArea());
+
+
+
+
+
+        Rectangle rectangle = new Rectangle(10,5);
+        System.out.println(rectangle);
+        System.out.println(rectangle.getPerimeter());
+
+        rectangle.resize(-50);
+        System.out.println(rectangle.getWidth() + " "+ rectangle.getLength());
+        System.out.println(rectangle.getPerimeter());
+
+
+    }
+
+}
